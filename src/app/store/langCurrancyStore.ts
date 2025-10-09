af1e8f87606524407e1eed6e15377e610923146f
IslamAsankojoev <isla.asankojoev@mail.ru>
2025-10-09 21:49:22 +0600

    lang and rates are parsisted

--- src/app/store/langCurrancyStore.ts
+++ src/app/store/langCurrancyStore.ts
@@ -1,22 +1,36 @@
 import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
 
 import { Code, Currency, ExchangeRate } from '../../shared/constants'
 
-export const langCurrancyStore = create<{
+interface LangCurrancyStore {
   lang: Code
   currency: Currency
   exchangeRates: ExchangeRate[]
   setLang: (lang: Code) => void
   setCurrency: (currency: Currency) => void
   setExchangeRates: (exchangeRates: ExchangeRate[]) => void
-}>((set) => ({
-  lang: Code.EN,
-  currency: Currency.USD,
-  exchangeRates: [],
-  setLang: (lang) => set({ lang }),
-  setCurrency: (currency) => set({ currency }),
-  setExchangeRates: (exchangeRates) =>
-    set({
-      exchangeRates: [...exchangeRates, { currency: Currency.ILS, rate: 1 }],
+}
+
+export const langCurrancyStore = create<LangCurrancyStore>()(
+  persist(
+    (set) => ({
+      lang: Code.EN,
+      currency: Currency.USD,
+      exchangeRates: [],
+      setLang: (lang) => set({ lang }),
+      setCurrency: (currency) => set({ currency }),
+      setExchangeRates: (exchangeRates) =>
+        set({
+          exchangeRates: [
+            ...exchangeRates,
+            { currency: Currency.ILS, rate: 1 },
+          ],
+        }),
     }),
-}))
+    {
+      name: 'langCurrancy',
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+)
