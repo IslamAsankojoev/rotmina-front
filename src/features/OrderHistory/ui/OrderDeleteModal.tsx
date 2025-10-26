f96102707a978f8033a88790acf64c543eb5d6c3
IslamAsankojoev <isla.asankojoev@mail.ru>
2025-10-27 00:54:22 +0600

    revalidate orders after delete

--- src/features/OrderHistory/ui/OrderDeleteModal.tsx
+++ src/features/OrderHistory/ui/OrderDeleteModal.tsx
@@ -12,6 +12,7 @@ import { DialogDescription, DialogTrigger } from '@radix-ui/react-dialog'
 import { useMutation } from '@tanstack/react-query'
 import { Trash2 } from 'lucide-react'
 import { toast } from 'sonner'
+import { useQueryClient } from '@tanstack/react-query'
 
 export interface OrderDeleteModalProps {
   open: boolean
@@ -24,11 +25,13 @@ export const OrderDeleteModal = ({
   onOpenChange,
   orderId,
 }: OrderDeleteModalProps) => {
+  const queryClient = useQueryClient()
   const { mutate: deleteOrder, isPending: isDeletingOrder } = useMutation({
     mutationFn: (orderId: string) => OrderService.deleteOrder(orderId),
     onSuccess: () => {
       toast.success('Order deleted successfully')
       onOpenChange(false)
+      queryClient.invalidateQueries({ queryKey: ['orders'] })
     },
     onError: () => {
       toast.error('Failed to delete order')
