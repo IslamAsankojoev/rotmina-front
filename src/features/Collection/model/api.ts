72e67c63c6862f63c3dc523d61be51da6dcc30b0
Islam Asankojoev <isla.asankojoev@mail.ru>
2025-11-26 11:57:13 +0600

    fixed collection top image

--- src/features/Collection/model/api.ts
+++ src/features/Collection/model/api.ts
@@ -4,5 +4,5 @@ import { CollectionOneResponse, CollectionResponse } from "./type"
 
 export const CollectionService = {
   getCollections: (): Promise<CollectionResponse> => api.get(apiMap.getCollections + '?populate=image').json(),
-  getCollection: (id: string): Promise<CollectionOneResponse> => api.get(apiMap.getCollection.replace(':id', id) + '?populate=image').json(),
+  getCollection: (id: string): Promise<CollectionOneResponse> => api.get(apiMap.getCollection.replace(':id', id) + '?populate=image&populate=top_image').json(),
 }
